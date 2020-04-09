c5b9ef16c27c21fb150e1e65cb2e2229d81e8bc9
Doziem <agbokennethchidoziem@gmail.com>
2020-04-09 20:03:29 +0100

    challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,33 +1,29 @@
 const covid19ImpactEstimator = (data) => {
-    const { reportedCases, timeToElapse, periodType } = data;
+  const { reportedCases, periodType } = data;
 
-    const currentlyInfectedForImpact = reportedCases * 10;
-    const currentlyInfectedForSevereImpact = reportedCases * 50;
+  const currentlyInfectedForImpact = reportedCases * 10;
+  const currentlyInfectedForSevereImpact = reportedCases * 50;
 
-    switch (periodType) {
-        case 'weeks':
-            timeToElapse *= 7;
-            break;
-        case 'months':
-            timeToElapse *= 7;
-            break;
+  if (periodType === 'weeks') {
+    data.timeToElapse *= 7;
+  } else if (periodType === 'months') {
+    data.timeToElapse *= 30;
+  }
+  const factor = Math.trunc(data.timeToElapse) / 3;
+  const infectionsByRequestedTimeForImpact = currentlyInfectedForImpact * (2 ** factor);
+  const infectionsByRequestedTimeForSevereImpact = currentlyInfectedForSevereImpact * (2 ** factor);
+
+  return {
+    data,
+    impact: {
+      currentlyInfected: currentlyInfectedForImpact,
+      infectionsByRequestedTime: infectionsByRequestedTimeForImpact
+    },
+    severeImpact: {
+      currentlyInfected: currentlyInfectedForSevereImpact,
+      infectionsByRequestedTime: infectionsByRequestedTimeForSevereImpact
     }
-
-    const factor = Math.trunc(timeToElapse) / 3;
-    const infectionsByRequestedTimeForImpact = currentlyInfectedForImpact * (2 ** factor);
-    const infectionsByRequestedTimeForSevereImpact = currentlyInfectedForSevereImpact * (2 ** factor);
-
-    return {
-        data,
-        impact: {
-            currentlyInfected: currentlyInfectedForImpact,
-            infectionsByRequestedTime: infectionsByRequestedTimeForImpact
-        },
-        severeImpact: {
-            currentlyInfected: currentlyInfectedForSevereImpact,
-            infectionsByRequestedTime: infectionsByRequestedTimeForSevereImpact
-        }
-    };
+  };
 };
 
 export default covid19ImpactEstimator;
