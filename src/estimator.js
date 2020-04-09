744ab0ba6aff7e21eba403a91b5d78f655ea2771
Doziem <agbokennethchidoziem@gmail.com>
2020-04-09 12:04:08 +0100

    challenge 1 completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,41 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const { region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds} = data;
+
+    const currentlyInfected = reportedCases * 10;
+    
+    const projectedInfection = () => {
+        let currentInfectionsByRequestedTime;
+        switch(periodType){
+            case "Days":
+                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 512/3);
+                break;
+            case "weeks":
+                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 512/7);
+                break;
+            case "months":
+                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 1024/30);
+                break;
+
+            default:
+                currentInfectionsByRequestedTime = null;   
+        }
+
+        return currentInfectionsByRequestedTime;
+    }
+    
+    const infectionsByRequestedTime = this.projectedInfection();
+
+    return {
+        data: {},
+        impact: {
+            currentlyInfected: reportedCases * 10,
+            infectionsByRequestedTime: this.projectedInfection,
+        },
+        severeImpact: {
+            currentlyInfected: reportedCases * 50,
+            infectionsByRequestedTime: this.projectedInfection,
+        }
+    }
+};
 
 export default covid19ImpactEstimator;
