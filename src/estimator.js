cdfd9dd6ec27e857c0714e8a86207c6b2c57f769
Doziem <agbokennethchidoziem@gmail.com>
2020-04-09 13:36:39 +0100

    challenge-1

--- src/estimator.js
+++ src/estimator.js
@@ -1,41 +1,41 @@
 const covid19ImpactEstimator = (data) => {
-    const { region, periodType, timeToElapse, reportedCases, population, totalHospitalBeds} = data;
+  const { periodType, reportedCases } = data;
 
-    const currentlyInfected = reportedCases * 10;
-    
-    const projectedInfection = () => {
-        let currentInfectionsByRequestedTime;
-        switch(periodType){
-            case "Days":
-                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 512/3);
-                break;
-            case "weeks":
-                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 512/7);
-                break;
-            case "months":
-                currentInfectionsByRequestedTime = Math.floor(currentlyInfected * 1024/30);
-                break;
+  const currentlyInfected = reportedCases * 10;
 
-            default:
-                currentInfectionsByRequestedTime = null;   
-        }
+  const projectedInfection = () => {
+    let currentInfectionsByRequestedTime;
+    switch (periodType) {
+      case 'Days':
+        currentInfectionsByRequestedTime = Math.floor((currentlyInfected * 512) / 3);
+        break;
+      case 'weeks':
+        currentInfectionsByRequestedTime = Math.floor((currentlyInfected * 512) / 7);
+        break;
+      case 'months':
+        currentInfectionsByRequestedTime = Math.floor((currentlyInfected * 1024) / 30);
+        break;
 
-        return currentInfectionsByRequestedTime;
+      default:
+        currentInfectionsByRequestedTime = null;
     }
-    
-    const infectionsByRequestedTime = this.projectedInfection();
 
-    return {
-        data: {},
-        impact: {
-            currentlyInfected: reportedCases * 10,
-            infectionsByRequestedTime: this.projectedInfection,
-        },
-        severeImpact: {
-            currentlyInfected: reportedCases * 50,
-            infectionsByRequestedTime: this.projectedInfection,
-        }
+    return currentInfectionsByRequestedTime;
+  };
+
+  // const infectionsByRequestedTime = projectedInfection();
+
+  return {
+    data,
+    impact: {
+      currentlyInfected: reportedCases * 10,
+      infectionsByRequestedTime: projectedInfection
+    },
+    severeImpact: {
+      currentlyInfected: reportedCases * 50,
+      infectionsByRequestedTime: projectedInfection
     }
+  };
 };
 
 export default covid19ImpactEstimator;
